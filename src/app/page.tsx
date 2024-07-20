aa8403df8084257734336155c0c366ab29bab8f9
eunsu <songess@naver.com>
2024-07-20 15:57:21 +0900

    feat: 병합시도

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Image from 'next/image';
+import GroupSVG from '../../public/svg/group.svg';
+import FirstSVG from '../../public/svg/first.svg';
+import SecondSVG from '../../public/svg/second.svg';
+import ThirdSVG from '../../public/svg/third.svg';
+import ScrollSVG from '../../public/svg/scroll.svg';
+import Group2SVG from '../../public/svg/group2.svg';
+import InstagramSVG from '../../public/svg/instagram.svg';
+import StarSVG from '../../public/svg/star.svg';
+import Link from 'next/link';
 import IphoneFifteenSVG from '../../public/svg/iPhone 15 Pro.svg';
 
 const reviews: {
@@ -53,7 +63,7 @@ const reviews: {
 export default function Home() {
   return (
     <main className="flex w-full h-full flex-col items-center overflow-y-scroll snap-y snap-mandatory relative">
-      <section className="w-full h-full bg-[#191F28] shrink-0 snap-start flex justify-center items-center gap-x-[49px]">
+      <section className="w-full h-full bg-[#191F28] shrink-0 snap-start flex justify-center items-center gap-[132px]">
         <div className="flex flex-col w-fit h-fit justify-start">
           <span>스포츠 여가의</span>
           <span>새로운 시작</span>
@@ -66,6 +76,132 @@ export default function Home() {
           </button>
         </div>
         <IphoneFifteenSVG />
+      </section>
+      <section className="z-20 w-full h-full bg-grey3 shrink-0 snap-start flex flex-col gap-[30px] justify-center items-center font-bold text-[34px] tracking-[-0.02em] text-grey5">
+        <p>
+          아직도 원하는 여가 사업체를 찾기 위해{' '}
+          <span className="text-bold text-grey6">일일이 검색</span>하시나요?
+        </p>
+        <p>
+          사업체마다{' '}
+          <span className="text-bold text-grey6">
+            요구하는 예약 방식이 다르거나, 아예 예약이 안 돼서
+          </span>{' '}
+          힘드셨나요?
+        </p>
+        <p>
+          <span className="text-bold text-grey6">예약 따로, 결제 따로..</span>{' '}
+          너무 번거롭지 않으신가요?
+        </p>
+        <p>
+          아직도 워크인으로 간 뒤 본인 자리가 날 때까지{' '}
+          <span className="text-bold text-grey6">마냥 기다리셨나요?</span>
+        </p>
+      </section>
+      <section className="w-full h-full bg-black bg-gradient-to-b from-[#CED3D6] to-white shrink-0 snap-start flex flex-col gap-[35px] justify-center items-center font-bold text-[55px] tracking-[-0.02em] text-grey7">
+        <p>걱정마세요,</p>
+        <p>
+          <span className="text-primary_orange1">TIG</span>가 모두
+          해결해드릴게요!
+        </p>
+      </section>
+      <section className="w-full h-full bg-[#F8F9FA] shrink-0 snap-start flex flex-col gap-[60px] justify-center items-center font-semibold text-[36px] tracking-[-0.02em] text-black px-[240px]">
+        <p>현재 많은 고객들이 TIG를 통해 여가를 즐기고 계십니다.</p>
+        <div className="w-3/4">
+          <Image
+            src="/png/dummy.png"
+            alt="tigVideo"
+            layout="responsive"
+            width={1440}
+            height={810}
+            sizes="(max-width: 1440px) 75vw, 1440px"
+          />
+        </div>
+      </section>
+      <section className="w-full h-full bg-white shrink-0 snap-start flex gap-[60px] justify-between items-center font-semibold text-[48px] leading-[70px] tracking-[-0.02em] text-black px-[240px]">
+        <div className="flex flex-col justify-between h-full py-[130px]">
+          <p className="w-fit h-[64px] p-[30px] border border-primary_orange1 rounded-[10px] font-semibold text-[28px] text-primary_orange1 flex justify-center items-center">
+            How to use?
+          </p>
+          <div className="flex flex-col gap-[30px]">
+            <p>1</p>
+            <p>
+              위치, 시간, 가격 등<br /> 필터 설정 후 필요한 정보만
+              <br /> 확인하세요!
+            </p>
+          </div>
+        </div>
+        <SecondSVG />
+      </section>
+      <section className="w-full h-full bg-[#FFFBF4] shrink-0 snap-start flex gap-[60px] justify-between items-center font-semibold text-[48px] leading-[70px] tracking-[-0.02em] text-black px-[240px]">
+        <ThirdSVG />
+        <div className="flex flex-col gap-[30px]">
+          <p>2</p>
+          <p>
+            예약부터 결제까지
+            <br /> 통일된 과정으로
+            <br /> 한번에 해결하세요!
+          </p>
+        </div>
+      </section>
+      <section className="w-full h-full bg-white shrink-0 snap-start flex gap-[60px] justify-between items-center font-semibold text-[48px] leading-[70px] tracking-[-0.02em] text-black px-[240px]">
+        <div className="flex flex-col gap-[30px]">
+          <p>3</p>
+          <p>
+            스포츠부터
+            <br /> 연극까지 한 번에
+            <br /> 예약하세요!
+          </p>
+        </div>
+        <SecondSVG />
+      </section>
+      <section className="w-full h-full bg-[#F5F6F7] shrink-0 snap-start flex gap-[70px] flex-col justify-center items-center px-[120px]">
+        <p className="w-fit h-[64px] p-[30px] border border-primary_orange1 rounded-[10px] font-semibold text-[28px] text-primary_orange1 flex justify-center items-center">
+          Customer Review
+        </p>
+        <div className="flex-wrap	w-full flex gap-[12px]">
+          {reviews.map((review, idx) => (
+            <div
+              key={idx}
+              className="w-calgap h-[180px] flex flex-col p-5 justify-around bg-white rounded-[10px]"
+            >
+              <p className="text-[18px] leading-[28px] font-medium tracking-[-0.02em] text-grey6">
+                {review.content}
+              </p>
+              <StarSVG />
+              <div className="flex gap-[10px] text-[18px] leading-[30px] font-medium tracking-[-0.02em] text-grey4">
+                <p>{review.name}</p>
+                <p>{review.info}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="w-full h-full bg-white shrink-0 snap-start flex flex-col justify-between items-center pt-[20px]">
+        <div className="w-3/4 relative">
+          <Image
+            src="/png/5.png"
+            alt="tigReccommened"
+            layout="responsive"
+            width={1200}
+            height={478}
+            sizes="(max-width: 1200px) 75vw, 1200px"
+          />
+          <p className="absolute left-[60px] top-[60px] text-white text-[30px] leading-[50px] tracking-[-0.02em]">
+            TIG는 2024 예비창업패키지 선정 기업으로
+            <br /> 정부와 함께 건강한 성장을 하고 있으며,
+            <br /> 법인 등록이 완료된 안전한 기업입니다.
+          </p>
+        </div>
+        <div className="w-full flex flex-col px-[240px] gap-[16px] bg-grey6 h-1/3 justify-center">
+          <Group2SVG className="shrink-0" />
+          <div className="font-medium text-[20px] leading-[40px] tracking-[-0.02em] text-grey5">
+            <p>서울특별시 광진구 광나루로 17길 14-4 201-1호</p>
+            <p>tel : 010-9159-6485</p>
+            <p>E-mail : 2290072@ewhain.net</p>
+          </div>
+          <InstagramSVG className="shrink-0" />
+        </div>
       </section>
     </main>
   );
